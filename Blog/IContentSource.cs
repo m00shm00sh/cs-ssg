ea8b31d97766b77f0e154e4194e20e750a39c257
andrey <andrey@moshbear.net>
2026-01-31 23:06:58 -0500

    Add support for a permissions model and support post listing

--- Blog/IContentSource.cs
+++ Blog/IContentSource.cs
@@ -1,8 +1,23 @@
+using OneOf;
+using OneOf.Types;
+
 namespace CsSsg.Blog;
 
 internal interface IContentSource
 {
-    Task<string?> GetContentOrNullAsync(string name, CancellationToken ct);
-    Task SetContentAsync(string name, string content, CancellationToken ct);
-    Task DeleteContentAsync(string name, CancellationToken ct);
+    public record struct Entry(string Title, string Name, DateTime LastModified);
+
+    public enum AccessLevel
+    {
+        None,
+        Read,
+        Write
+    }
+    
+    public Task<AccessLevel?> GetPermissionsForContentAsync(Guid? userId, string name, CancellationToken ct);
+    Task<IEnumerable<Entry>> GetAvailableContentAsync(Guid? userId, DateTimeOffset beforeOrAt, int limit,
+        CancellationToken ct);
+    Task<string?> GetContentAsync(Guid? userId, string name, CancellationToken ct);
+    Task<bool> SetContentAsync(Guid? userId, string name, string content, CancellationToken ct);
+    Task<bool> DeleteContentAsync(Guid? userId, string name, CancellationToken ct);
 }
--- Blog/RoutingExtensions.cs
+++ Blog/RoutingExtensions.cs
@@ -1,5 +1,8 @@
+using System.Globalization;
 using System.Net.Mime;
+using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using ZiggyCreatures.Caching.Fusion;
 
 namespace CsSsg.Blog;
@@ -11,24 +14,58 @@ internal static class RoutingExtensions
         public void AddBlogRoutes()
         {
             app.MapGet("/blog/{name}",
-                async (string name, IContentSource source, IFusionCache cache,
-                    CancellationToken ct) =>
+                async (string name, ClaimsPrincipal? auth, IContentSource source, MarkdownHandler md,
+                    IFusionCache cache, CancellationToken ct) =>
                 {
-                    var contents = await cache.GetOrSetAsync(
-                        name, async _ =>
-                        {
-                            var contents = await source.GetContentOrNullAsync(name, ct);
-                            if (contents is null)
-                                return null;
-                            var (title, article) = MarkdownToHtml.RenderMarkdownToHtml(contents);
-                            return HtmlDocumentMaker.ConvertHtmlContentsToFullPage(title, article);
-                        },
-                        token: ct);
-                    return contents is not null
-                        ? Results.Text(contents, MediaTypeNames.Text.Html, contentEncoding: Encoding.UTF8)
-                        : TypedResults.NotFound();
+                    Guid? uidFromCookie = null;
+                    var canAccess = await cache.GetOrSetAsync(
+                        $"access/{uidFromCookie}/{name}",
+                        async _ => await source.GetPermissionsForContentAsync(uidFromCookie, name, ct),
+                        tags: ["access"], token: ct);
+                    switch (canAccess)
+                    {
+                        case null:
+                            return TypedResults.NotFound();
+                        case IContentSource.AccessLevel.None:
+                            return TypedResults.Forbid();
+                        case IContentSource.AccessLevel.Read or IContentSource.AccessLevel.Write:
+                            var contents = await cache.GetOrSetAsync(
+                                $"html/{name}", async _ =>
+                                {
+                                    var contents = await source.GetContentAsync(uidFromCookie, name, ct);
+                                    // it could fail due to permissions but because we're caching the step after
+                                    // the access check, it's going to be a lookup failure as if resource doesn't exist
+                                    if (contents is null)
+                                        return null;
+                                    var (title, article) = md.RenderMarkdownToHtml(contents, name, ct);
+                                    return HtmlRenderer.ConvertHtmlArticleContentsToFullPage(title, article,
+                                        addEditPostButton: canAccess == IContentSource.AccessLevel.Write);
+                                },
+                                tags: ["html"],
+                                token: ct);
+                            return contents is not null
+                                ? Results.Text(contents, MediaTypeNames.Text.Html, contentEncoding: Encoding.UTF8)
+                                : TypedResults.NotFound();
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(canAccess), canAccess, null);
+                    }
                 });
-            
+            app.MapGet("/blog",
+                async (ClaimsPrincipal? auth, IContentSource source, IFusionCache cache,
+                    CancellationToken ct, [FromQuery] int limit = 10, [FromQuery] string? beforeOrAt = null) =>
+                {
+                    Guid? uidFromCookie = null;
+                    var date = beforeOrAt is null ? DateTime.UtcNow : DateTime.Parse(beforeOrAt, 
+                        null, DateTimeStyles.RoundtripKind);
+                    var listing = await cache.GetOrSetAsync(
+                        $"listing+html/{uidFromCookie};{date};limit",
+                        async _ => HtmlRenderer.RenderPostListingToHtmlBodyElements(
+                                await source.GetAvailableContentAsync(uidFromCookie, date, limit, ct),
+                                addNewPostButton: uidFromCookie != null),
+                        tags: ["listing", "html"], token: ct);
+                    var page = HtmlRenderer.ConvertHtmlContentsToFullPage("Posts", listing);
+                    return Results.Text(page, MediaTypeNames.Text.Html, Encoding.UTF8);
+                });
             app.MapGet("/", () => Results.Redirect("/blog/index"));
             app.MapGet("/contact", () => Results.Redirect("/blog/contact"));
         }
