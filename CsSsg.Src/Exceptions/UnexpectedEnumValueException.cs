c59467879947db0d00aeb6916c9fbf1d200b67c3
andrey <andrey@moshbear.net>
2026-02-23 04:08:36 -0500

    add custom exception class for unexpected enum value

---
+++ CsSsg.Src/Exceptions/UnexpectedEnumValueException.cs
@@ -0,0 +1,14 @@
+namespace CsSsg.Src.Exceptions;
+
+
+public class UnexpectedEnumValueException(string message) : InvalidOperationException(message)
+{
+    public static UnexpectedEnumValueException Create<TEnum>(TEnum? value) where TEnum : struct, Enum
+        => new($"type: {typeof(TEnum).Name}, value: {value}");
+
+    public static void VerifyOrThrow<TEnum>(TEnum? value) where TEnum : struct, Enum
+    {
+        if (value.HasValue && Enum.IsDefined(value.Value)) return;
+        throw Create(value);
+    }
+}
