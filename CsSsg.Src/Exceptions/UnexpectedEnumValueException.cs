c27b51ab4ee2ba54e9a3359700396186f1733f63
andrey <andrey@moshbear.net>
2026-02-25 20:14:44 -0500

    UnexpectedEnumValueException: handle null with explicit string value

--- CsSsg.Src/Exceptions/UnexpectedEnumValueException.cs
+++ CsSsg.Src/Exceptions/UnexpectedEnumValueException.cs
@@ -4,7 +4,7 @@ namespace CsSsg.Src.Exceptions;
 public class UnexpectedEnumValueException(string message) : InvalidOperationException(message)
 {
     public static UnexpectedEnumValueException Create<TEnum>(TEnum? value) where TEnum : struct, Enum
-        => new($"type: {typeof(TEnum).Name}, value: {value}");
+        => new($"type: {typeof(TEnum).Name}, value: {value.ToString() ?? "(null)"}");
 
     public static void VerifyOrThrow<TEnum>(TEnum? value) where TEnum : struct, Enum
     {
