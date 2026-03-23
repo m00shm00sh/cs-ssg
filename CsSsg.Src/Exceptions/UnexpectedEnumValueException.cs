d298489e3d9b84d36d441b3b7a867a701c175345
andrey <andrey@moshbear.net>
2026-03-23 04:51:11 -0400

    UnexpectedEnumValueException: accept non-nullable enum

--- CsSsg.Src/Exceptions/UnexpectedEnumValueException.cs
+++ CsSsg.Src/Exceptions/UnexpectedEnumValueException.cs
@@ -6,9 +6,18 @@ public class UnexpectedEnumValueException(string message) : InvalidOperationExce
     public static UnexpectedEnumValueException Create<TEnum>(TEnum? value) where TEnum : struct, Enum
         => new($"type: {typeof(TEnum).Name}, value: {value.ToString() ?? "(null)"}");
 
+    public static UnexpectedEnumValueException Create<TEnum>(TEnum value) where TEnum : struct, Enum
+        => new($"type: {typeof(TEnum).Name}, value: {value}");
+
     public static void VerifyOrThrow<TEnum>(TEnum? value) where TEnum : struct, Enum
     {
         if (value.HasValue && Enum.IsDefined(value.Value)) return;
+        throw Create(value);
+    }
+
+    public static void VerifyOrThrow<TEnum>(TEnum value) where TEnum : struct, Enum
+    {
+        if (Enum.IsDefined(value)) return;
         throw Create(value);
     }
 }
