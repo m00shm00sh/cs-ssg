b6966df8bf2b3111c0e75fe041076d38e534e6e0
andrey <andrey@moshbear.net>
2026-03-30 04:22:51 -0400

    src: static routing: correct for mvctest setting content path to project root instead of solution root

--- CsSsg.Src/Static/RoutingExtensions.cs
+++ CsSsg.Src/Static/RoutingExtensions.cs
@@ -8,13 +8,17 @@ internal static class RoutingExtensions
     {
         public void AddStaticRoutes(string prefix)
         {
-            
+            // we need to cook root path a bit when invoked from integration tests; 
+            // this should detect if running under test and adjust accordingly
+            var applicationName = typeof(RoutingExtensions).Assembly.GetName().Name
+                                  ?? throw new InvalidOperationException("unexpected: null assembly name");
+            var contentRootPath = app.Environment.ContentRootPath;
+            var rootPath = Path.Combine(contentRootPath, applicationName);
+            if (contentRootPath.EndsWith(applicationName))
+                rootPath = Path.Combine(contentRootPath, "..", applicationName);
             app.UseStaticFiles(new StaticFileOptions
             {
-            
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(app.Environment.ContentRootPath, "public")
-                ),
+                FileProvider = new PhysicalFileProvider(rootPath),
                 RequestPath = $"/{prefix}"
             });
         }
