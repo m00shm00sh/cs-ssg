8d705937de01bf891695f8a438693420897e3fdf
andrey <andrey@moshbear.net>
2026-04-06 03:56:33 -0400

    test coverage: begin post[html]

---
+++ CsSsg.Test.HtmlApi/Post/ApiTests.cs
@@ -0,0 +1,115 @@
+using System.Net;
+using KotlinScopeFunctions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+using Request = CsSsg.Src.User.Request;
+
+using CsSsg.Test.Db;
+
+using CsSsg.Test.HtmlApi.Fixture;
+using CsSsg.Test.HtmlApi.Html;
+using CsSsg.Test.HtmlApi.Http;
+
+namespace CsSsg.Test.HtmlApi.Post;
+
+public class ApiTests : IClassFixture<PostgresFixture>
+{
+#region scaffolding
+    private readonly ILogger<ApiTests> _logger;
+    private readonly HttpClient _client;
+    
+    // this must be static for adequate sharing as xunit seems to be producing multiple instances
+    private static int _userCounter;
+    private static int _postCounter;
+
+    public ApiTests(PostgresFixture fixture, ITestOutputHelper outputHelper)
+    {
+        var factory = new WebAppFactory(outputHelper, fixture);
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions()
+        {
+            AllowAutoRedirect = false
+        });
+        // the logger for the test function itself, not to be confused with the logger configured for asp.net up above
+        _logger = LoggerFactory.Create(builder => builder.AddXUnit(outputHelper)).CreateLogger<ApiTests>();
+    }
+    
+    private static int _nextUserId =>  Interlocked.Increment(ref _userCounter);
+    private static int _nextPostId =>  Interlocked.Increment(ref _postCounter);
+
+    private record struct LoggedInUser(Request Details, string SessionCookie);
+    
+    private async Task<LoggedInUser> _nextSignedUpUserAsync(CancellationToken token)
+    {
+        var user = _nextDetails();
+        var response = await _client.PostProtectedFormAsync(
+            "/auth/login", "name=signupButton".AsFormSubmitSelector(),
+            new Dictionary<string, string>
+            {
+                ["email"] =  user.Email,
+                ["password"] =  user.Password,
+            }, token);
+        var sessionCookie = response.TryGetSessionCookie();
+        Assert.False(string.IsNullOrEmpty(sessionCookie));
+        return new LoggedInUser(user, sessionCookie);
+    }
+    
+    private Request _nextDetails()
+    {
+        var next = _nextUserId;
+        var nextUserId = $"{next:00}";
+        _logger.LogInformation("Create user {nextUserId}", nextUserId);
+        return new Request(Email: $"{nextUserId}@test!html!post", Password: $"test{nextUserId}");
+    }
+    
+#endregion
+#region Create post
+    [Fact]
+    public async Task TestCreatePost_RequiresAuth()
+    {
+        var response = await _client.GetAsync("/blog/-new");
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task TestSignup_ThenCreatePost_RequiresAntiforgery()
+    {
+        var (_, session) = await _nextSignedUpUserAsync(CancellationToken.None);
+
+        _logger.LogInformation("Create post");
+        var response = await _client.PostProtectedFormAsync(
+            "/blog/-new", "name=submitButton".AsFormSubmitSelector(),
+            new HeaderDictionary
+            {
+                ["Cookie"] = session
+            }, new Dictionary<string, string>
+            {
+                ["title"] = $"Hello {_nextPostId}",
+                ["contents"] = "# World"
+            }, skipCsrf: true);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.Contains("antiforgery", await response.Content.ReadAsStringAsync());
+    }
+    
+    [Fact]
+    public async Task TestSignup_ThenCreatePost()
+    {
+        var (_, session) = await _nextSignedUpUserAsync(CancellationToken.None);
+
+        _logger.LogInformation("Create post");
+        var response = await _client.PostProtectedFormAsync(
+            "/blog/-new", "name=submitButton".AsFormSubmitSelector(),
+            new HeaderDictionary
+            {
+                ["Cookie"] = session
+            }, new Dictionary<string, string>
+            {
+                ["title"] = $"Hello {_nextPostId}",
+                ["contents"] = "# World"
+            });
+        Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+    }
+#endregion
+}
