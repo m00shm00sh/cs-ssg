497148b768007e45bc3f942a137d8160208eed6f
andrey <andrey@moshbear.net>
2026-04-06 19:27:35 -0400

    test-coverage: exclude autogenerated sources from coverage

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dotnet test --collect:"XPlat Code Coverage"
 covxml=$(ls -t $(find CsSsg.Test{,.HtmlApi}/TestResults/ -name coverage.cobertura.xml) | head -2 | tr \\n \;)
-reportgenerator -reports:$covxml -targetdir:coveragereport -reporttypes:Html
+reportgenerator -reports:$covxml -targetdir:coveragereport -reporttypes:Html -filefilters:'-*/obj/*'
 xdg-open coveragereport/index.html
