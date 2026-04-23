60830ae3ca33a7dfce0dd12e2b852f3df38b42c4
andrey <andrey@moshbear.net>
2026-04-23 15:58:48 -0400

    refactor: src/static/routingextensions: split content root path fixup and static middleware setup

--- CsSsg.Src/Static/RoutingExtensions.cs
+++ CsSsg.Src/Static/RoutingExtensions.cs
@@ -4,19 +4,27 @@ namespace CsSsg.Src.Static;
 
 internal static class RoutingExtensions
 {
-    extension(WebApplication app)
+    extension(IWebHostEnvironment env)
     {
-        public void AddStaticRoutes(string prefix)
+        internal string ResolveSolutionContentRootPath()
         {
             // we need to cook root path a bit when invoked from integration tests; 
             // this should detect if running under test and adjust accordingly
             var applicationName = typeof(RoutingExtensions).Assembly.GetName().Name
                                   ?? throw new InvalidOperationException("unexpected: null assembly name");
-            var contentRootPath = app.Environment.ContentRootPath;
+            var contentRootPath = env.ContentRootPath;
             if (contentRootPath.EndsWith(applicationName))
-                contentRootPath = Path.Combine(contentRootPath, "..", "public");
-            else
-                contentRootPath = Path.Combine(contentRootPath, "public");
+                contentRootPath = Path.Combine(contentRootPath, "..");
+            return contentRootPath;
+        }
+    }
+    
+    extension(WebApplication app)
+    {
+       
+        public void AddStaticRoutes(string prefix)
+        {
+            var contentRootPath = Path.Combine(app.Environment.ResolveSolutionContentRootPath(), "public");
             app.Logger.LogInformation($"loading static from {contentRootPath}");
             app.UseStaticFiles(new StaticFileOptions
             {
