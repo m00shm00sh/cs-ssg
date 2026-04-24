340783167cde502bdf0d3c3ac1d37649418674a1
andrey <andrey@moshbear.net>
2026-04-24 19:32:53 -0400

    consoleloader: move main into Program.cs

--- CsSsg.ConsoleLoader/Program.cs
+++ CsSsg.ConsoleLoader/Program.cs
@@ -1,3 +1,25 @@
-﻿using CsSsg.ConsoleLoader;
+﻿using CsSsg.Src.User;
 
-DirLoaderProgram.Run(args);
+using CsSsg.ConsoleLoader;
+using CsSsg.ConsoleLoader.Worker;
+
+CancellationTokenSource canceller = new();
+Console.CancelKeyPress += (_, e) =>
+{
+    canceller.Cancel();
+    e.Cancel = true;
+};
+
+var environment = ConsoleAppExtensions.EnvironmentWithLoggerFactory.Value;
+var config = ConsoleAppExtensions.Configuration.Value;
+
+var login = new Request
+{
+    Email = config.GetFromEnvironmentOrConfig("LOADER_EMAIL", "Loader:Email"),
+    Password = config.GetFromEnvironmentOrConfig("LOADER_PASS", "Loader:Password")
+};
+
+var client = new Client(environment.LoggerFactory, login, "http://localhost:8888");
+
+var postWorker = new PostsWorker(environment.LoggerFactory, environment, client);
+await postWorker.DoDirectoryAsync("content", canceller.Token);
