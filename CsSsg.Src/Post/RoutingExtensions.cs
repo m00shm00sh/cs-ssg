84a93654a04937494059d4a83ffe5e1feb970ff6
andrey <andrey@moshbear.net>
2026-06-13 11:56:03 -0400

    Revert "add revision enumeration pagination"
    This reverts commit ef2379107f02b196a454e8ea6a8efd2e3d84bcfa.

--- CsSsg.Src/Post/RoutingExtensions.cs
+++ CsSsg.Src/Post/RoutingExtensions.cs
@@ -150,8 +150,7 @@ internal static partial class RoutingExtensions
         string name, Guid uid, IManageCommand.PostTags tags, ConcurrencyToken cToken,
         AppDbContext repo, IFusionCache cache, CancellationToken token)
     {
-        var revisionsSpecifier = new EnumerateRevisionsSpecifier();
-        var revisionsResult = await repo.GetRevisionsForContentAsync(name, revisionsSpecifier, cToken, token);
+        var revisionsResult = await repo.GetRevisionsForContentAsync(name, cToken, token);
         var revisions = revisionsResult.Match(list => list,
             f => throw new FailureException(f));
         
